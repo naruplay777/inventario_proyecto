73c843d73f3ce8cf114b41cf18865a3a0b12c1b6
naruplay777 <narugonzalez13@gmail.com>
2025-01-09 21:39:34 -0400

    Revert "dieno"
    This reverts commit f2395b226bb1ddbf8dd09afaae4cf8b009f5fb16.

--- Form1.cs
+++ Form1.cs
@@ -8,7 +8,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
-using System.Runtime.InteropServices;
 
 
 
@@ -23,12 +22,9 @@ namespace inventario_proyecto
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            // Puedes inicializar algo aquí si lo necesitas.
         }
-        [DllImport("user32.Dll", EntryPoint = "ReleaseCapture")]
-        private extern static void ReleaseCapture();
-        [DllImport("user32.Dll", EntryPoint = "SendMessage")]
-        private extern static void SendMessage(System.IntPtr hWnd, int wMsg, int wParam, int lParam);
+
         // Método para probar la conexión al hacer clic en un botón.
 
         private void btnTestConnection_Click_1(object sender, EventArgs e)
@@ -58,61 +54,10 @@ namespace inventario_proyecto
             FormularioProductos formulario1 = new FormularioProductos();
 
             // Mostrar el Formulario1
-            //formulario1.Show();
+            formulario1.Show();
 
             // Cerrar Formulario2
-            //this.Hide();
-
-            AbrirFormButon(new FormularioProductos());
-        }
-
-        private void pictureCerrar_Click(object sender, EventArgs e)
-        {
-            Application.Exit();
-        }
-
-        private void pictureMinimizar_Click(object sender, EventArgs e)
-        {
-            this.WindowState = FormWindowState.Minimized;
-        }
-
-        private void pictureMaximizar_Click(object sender, EventArgs e)
-        {
-            this.WindowState = FormWindowState.Maximized;
-            pictureMaximizar.Visible = false;
-            pictureRetaurar.Visible = true;
-        }
-
-        private void pictureRetaurar_Click(object sender, EventArgs e)
-        {
-            this.WindowState = FormWindowState.Normal;
-            pictureRetaurar.Visible = false;
-            pictureMaximizar.Visible = true;
-        }
-
-        private void panelBarra_MouseDown(object sender, MouseEventArgs e)
-        {
-            ReleaseCapture();
-            SendMessage(this.Handle, 0x112, 0xf012, 0);
-        }
-
-        private void AbrirFormButon(object formbuton)
-        {
-
-            if (this.panelContenedor.Controls.Count > 0)
-                this.panelContenedor.Controls.RemoveAt(0);
-            Form fb = formbuton as Form;
-            fb.TopLevel = false;
-            fb.Dock = DockStyle.Fill;
-            this.panelContenedor.Controls.Add(fb);
-            this.panelContenedor.Tag = fb;
-            fb.Show();
-
-
-        }
-
-        private void panelBarra_Paint(object sender, PaintEventArgs e)
-        {
+            this.Hide();
 
         }
     }
