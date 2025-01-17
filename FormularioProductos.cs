03b2059a039669085f5891a176a79afe6cfa484d
frankelvisr <frankelvisramos20@gmail.com>
2025-01-16 23:14:19 -0400

    librerias

--- FormularioProductos.cs
+++ FormularioProductos.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
 
 namespace inventario_proyecto
 {
