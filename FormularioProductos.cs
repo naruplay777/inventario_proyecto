f7ebcd31f565a40b064d3d033055e03d57b5d9be
frankelvisr <frankelvisramos20@gmail.com>
2025-01-24 22:44:31 -0400

    quedo mejor

--- FormularioProductos.cs
+++ FormularioProductos.cs
@@ -147,84 +147,97 @@ namespace inventario_proyecto
 
         private void btnGenerarpdf_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Deceas decargar el documento.", "Exito", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
-           FileStream fs = new FileStream("C:\\Users\\Personal\\Documents\\PDFGenerado.pdf" , FileMode.Create);
-            Document doc = new Document(PageSize.LETTER, 3, 3, 3, 3);
-            PdfWriter pw =  PdfWriter.GetInstance(doc, fs);
+            DialogResult result = MessageBox.Show("¿Deseas descargar el documento?", "Éxito", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
 
-            doc.Open();
+            if (result == DialogResult.Yes)
+            {
+                // Crear el archivo PDF  
+                using (FileStream fs = new FileStream("C:\\Users\\Personal\\Documents\\PDFGenerado.pdf", FileMode.Create))
+                {
+                    Document doc = new Document(PageSize.LETTER, 3, 3, 3, 3);
+                    PdfWriter pw = PdfWriter.GetInstance(doc, fs);
 
-            //titulo del pdf
-            doc.AddAuthor("Wiredbox");
-            doc.AddTitle("CYROP");
+                    doc.Open();
 
-            //definimos fuente
-            iTextSharp.text.Font standarfint = new iTextSharp.text.Font(iTextSharp.text.Font.FontFamily.TIMES_ROMAN, 12, iTextSharp.text.Font.NORMAL, BaseColor.BLACK);
+                    // Título del PDF  
+                    doc.AddAuthor("Wiredbox");
+                    doc.AddTitle("CYROP");
 
-            //se crea el titulo
-            Paragraph title = new Paragraph("Titulo del documento");
+                    // Definimos fuente  
+                    iTextSharp.text.Font standarfint = new iTextSharp.text.Font(iTextSharp.text.Font.FontFamily.TIMES_ROMAN, 12, iTextSharp.text.Font.NORMAL, BaseColor.BLACK);
 
-            // Centrar el título  
-            title.Alignment = Element.ALIGN_CENTER;
+                    // Cargar la imagen  
+                    string imagePath = "C:\\Users\\Personal\\Documents\\1000036045.jpg"; 
+                    iTextSharp.text.Image img = iTextSharp.text.Image.GetInstance(imagePath);
+                    img.ScaleToFit(60f, 60f); // Ajustar el tamaño de la imagen  
 
-            // Agregar el título al documento  
-            doc.Add(title);
+                    // Agregar la imagen al documento  
+                    img.Alignment = Element.ALIGN_CENTER;
+                    doc.Add(img);
 
-            // Agregar un salto de línea  
-            doc.Add(Chunk.NEWLINE);
+                    // Se crea el título  
+                    Paragraph title = new Paragraph("Título del documento", standarfint);
+                    title.Alignment = Element.ALIGN_CENTER;
 
+                    // Agregar el título al documento  
+                    doc.Add(title);
 
-            //emcabezado de columnas 
+                    // Agregar un salto de línea  
+                    doc.Add(Chunk.NEWLINE);
 
-            PdfPTable tb = new PdfPTable(6);
-            tb.WidthPercentage = 90;
+                    // Encabezado de columnas   
+                    PdfPTable tb = new PdfPTable(6);
+                    tb.WidthPercentage = 90;
 
-            //Configuracion del titulo de columnas 
+                    // Configuración del título de columnas   
+                    PdfPCell clID = new PdfPCell(new Phrase("ID", standarfint));
+                    clID.BorderWidth = 1;
+                    clID.BorderWidthBottom = 0.75f;
 
-            PdfPCell clID = new PdfPCell(new Phrase("ID", standarfint));
-            clID.BorderWidth = 1;
-            clID.BorderWidthBottom = 0.75f;
+                    PdfPCell clNombre = new PdfPCell(new Phrase("Nombre", standarfint));
+                    clNombre.BorderWidth = 1;
+                    clNombre.BorderWidthBottom = 0.75f;
 
-            PdfPCell clNombre = new PdfPCell(new Phrase("Nombre", standarfint));
-            clNombre.BorderWidth = 1;
-            clNombre.BorderWidthBottom = 0.75f;
+                    PdfPCell clUnidad = new PdfPCell(new Phrase("UnidadMedia", standarfint));
+                    clUnidad.BorderWidth = 1;
+                    clUnidad.BorderWidthBottom = 0.75f;
 
-            PdfPCell clUnidad = new PdfPCell(new Phrase("UnidadMedia", standarfint));
-            clUnidad.BorderWidth = 1;
-            clUnidad.BorderWidthBottom = 0.75f;
+                    PdfPCell clActual = new PdfPCell(new Phrase("StockActual", standarfint));
+                    clActual.BorderWidth = 1;
+                    clActual.BorderWidthBottom = 0.75f;
 
-            PdfPCell clActual = new PdfPCell(new Phrase("StockActual", standarfint));
-            clActual.BorderWidth = 1;
-            clActual.BorderWidthBottom = 0.75f;
+                    PdfPCell clMinimo = new PdfPCell(new Phrase("StockMinimo", standarfint));
+                    clMinimo.BorderWidth = 1;
+                    clMinimo.BorderWidthBottom = 0.75f;
 
-            PdfPCell clMinimo = new PdfPCell(new Phrase("StockMinimo", standarfint));
-            clMinimo.BorderWidth = 1;
-            clMinimo.BorderWidthBottom = 0.75f;
+                    PdfPCell clCategoria = new PdfPCell(new Phrase("Categoria", standarfint));
+                    clCategoria.BorderWidth = 1;
+                    clCategoria.BorderWidthBottom = 0.75f;
 
-            PdfPCell clCategoria = new PdfPCell(new Phrase("Categoria", standarfint));
-            clCategoria.BorderWidth = 1;
-            clCategoria.BorderWidthBottom = 0.75f;
+                    // Agregamos las columnas a la tabla  
+                    tb.AddCell(clID);
+                    tb.AddCell(clNombre);
+                    tb.AddCell(clUnidad);
+                    tb.AddCell(clActual);
+                    tb.AddCell(clMinimo);
+                    tb.AddCell(clCategoria);
 
-            //Agregamos las columnas a la tabla
-            tb.AddCell(clID);
-            tb.AddCell(clNombre);
-            tb.AddCell(clUnidad);
-            tb.AddCell(clActual);
-            tb.AddCell(clMinimo);
-            tb.AddCell(clCategoria);
+                    // Agregar datos aquí (ejemplo comentado)  
+                    /* foreach (var mercancia in productos )  
+                     {  
+                         clID = new PdfPCell(new Phrase(mercancia.ID.ToString(), standarfint));  
+                         tb.AddCell(clID);  
+                     }*/
 
-            //Agregamos datos aqui se aplica lo que es con la base de dato para los datos que se agreguen 
-            /* foreach (var mercancia in productos )
-             {
-                 clID = new PdfPCell(new Phrase(mercancia.ID.ToString, standarfint));
-             }*/
+                    doc.Add(tb);
 
-            doc.Add(tb);
+                    // Cerrar el documento  
+                    doc.Close();
+                    pw.Close();
 
-            doc.Close();
-            pw.Close();
-
-            MessageBox.Show("Documento generado satisfactoriamente", "Exitos", MessageBoxButtons.OK,MessageBoxIcon.Information);
+                    MessageBox.Show("Documento generado satisfactoriamente", "Éxitos", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
         }
     }
 }
